dda9526df925eed133a01d85ded0ca2389b2c1b4
35191 <35191@DESKTOP-M9MO6AK>
2024-04-23 20:18:35 +0100

    Adicionar arquivos de projeto.

---
+++ A00_Utility/Utility.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A00_Utility
+{
+    public class Utility
+    {
+        public static void SetUnicodeConsole()
+        {
+
+            // Console.WriteLine("á Á à À ã Ã â Â ç Ç º ª");
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            // Console.WriteLine("á Á à À ã Ã â Â ç Ç º ª");
+
+        }
+
+        public static void WriteTitle(string title)
+        {
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(new string('-',60));
+            Console.WriteLine(title.ToUpper());
+            Console.WriteLine(new string('-', 60));
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine();
+
+        }
+
+        public static void WriteMessage(string message, string beginMessage = "", string endMessage = "")
+        {
+
+            Console.Write($"{beginMessage}{message}{endMessage}");
+
+
+        }
+
+        public static void TerminateConsole()
+        {
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("\n\n\n\nPrime qualquer tecla para terminares.");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.ReadKey();
+            Console.Clear();
+
+        }
+
+
+
+
+    }
+}
---
+++ A01_CalculoDoIMC/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using A00_Utility;
+
+namespace A01_CalculoDoIMC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Utility.SetUnicodeConsole();
+
+            Utility.WriteTitle("Calculadora do Índice de Massa Corporal (IMC)");
+
+            #region 1. Variaveis
+
+            double peso, altura, iMC;
+
+            #endregion
+
+            #region 2. Leitura de Valores
+
+            // Altura
+            Utility.WriteMessage("Qual é a sua altura? ");
+            altura = Convert.ToDouble(Console.ReadLine());
+
+            // Peso
+            Utility.WriteMessage("Qual é o seu Peso? ");
+            peso = Convert.ToDouble(Console.ReadLine());
+
+            #endregion
+
+            #region 3. Calculo do IMC
+
+            iMC = Math.Round(peso / (altura * altura), 2);
+            Utility.WriteMessage($"O seu Índice de Massa Corcoral é de {iMC.ToString()}");
+
+            #endregion
+
+            #region 4. Explicação do Resultado do IMC
+
+            if(iMC < 18.5)
+            {
+                Utility.WriteMessage("\nO seu IMC é considerado PESO CORPORAL BAIXO, assim deve procurar a ajuda de um profissional de nutrição para ganhar peso de forma saudável.");
+            }
+            else if(iMC > 18.5 && iMC < 24.9)
+            {
+                Utility.WriteMessage("\nO seu IMC é considerado PESO CORPORAL NORMAL, assim deve continuar a praticar uma alimentação saudavel para se manter saudável.");
+            }
+            else if(iMC > 24.9 && iMC < 29.9)
+            {
+                Utility.WriteMessage("\nO seu IMC é considerado PESO CORPORAL EXCESSIVO, assim deve procurar a ajuda de um profissional de nutrição para perder peso de forma sauldável.");
+            }
+            else if(iMC > 29.9 && iMC < 34.9)
+            {
+                Utility.WriteMessage("\nO seu IMC é considerado OBESIDADE MODERADA, assim deve procurar a ajuda de um profissional de nutrição para perder peso de forma sauldável.");
+            }
+            else if (iMC > 34.9 && iMC < 39.9)
+            {
+                Utility.WriteMessage("\nO seu IMC é considerado OBESIDADE GRAVE, assim deve procurar a ajuda de um profissional de nutrição para perder peso de forma sauldável.");
+            }
+            else if (iMC > 39.9 && iMC < 49.9)
+            {
+                Utility.WriteMessage("\nO seu IMC é considerado OBESIDADE MÓRBIDA, assim deve procurar a ajuda de um profissional de nutrição para perder peso de forma sauldável.");
+            }
+            else
+            {
+                Utility.WriteMessage("\nO seu IMC é considerado SUPEROBESIDADE, assim deve procurar a ajuda de um profissional de nutrição para perder peso de forma sauldável.");
+            }
+
+            #endregion
+
+            // Pausar a Consola
+            Utility.TerminateConsole();
+
+        }
+    }
+}
+
